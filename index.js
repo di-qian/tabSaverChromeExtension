47343c062b2e57d40c0f0126b3dcdb54c5f663ed
Di Qian <diqian@gmail.com>
2022-01-14 16:14:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+let myLeads = [];
+let oldLeads =[];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const deleteBtn = document.getElementById("delete-btn");
+const saveBtn = document.getElementById("save-btn");
+const ulEl= document.getElementById("ul-el");
+
+const leadsFromLocalstorage = JSON.parse(localStorage.getItem("myLeads"));
+
+
+if(leadsFromLocalstorage){
+    myLeads = leadsFromLocalstorage;
+    render(myLeads);
+}
+
+const tabs = [{url: "https://www.linkedin.com/in/per-harald-borgen/"}];
+
+inputBtn.addEventListener("click", function(){
+
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+
+})
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+saveBtn.addEventListener("click", function(){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads",JSON.stringify(myLeads));
+        render(myLeads);
+    })
+
+
+    
+})
+
+function render(leads){
+    let listItems = "";
+    for (let i=0;i<leads.length;i++){
+        listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
+            </a>
+        </li>
+        `;
+    }
+
+    ulEl.innerHTML = listItems;
+}
+
+
+
+
